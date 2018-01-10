2f3d599f3dcd8b7c927fb1e33669de538265beac
Abhishek Piedy <abhishek.piedy@gmail.com>
2018-01-10 16:53:15 -0500

    Sidebar styles

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -9,37 +9,48 @@ export default class Sidebar extends React.Component {
     render() {   
         return(
             <Col className="sidebar ">
+                <div className="appName">
+                    <NavLink to="/app">React Application</NavLink>
+                </div>
                 <div className="userDetailsWrapper">
-                    <h4 className = "appName"><NavLink to="/app">React Application</NavLink> </h4>
                     <div className="userImg">
                         <img className = "img-circle" src="http://www.oiioproperty.com/Images/article/2016051402425466.png" alt="Profile User"/>
-                        
-                        <p className="userName">John Alexander</p>
-                        <p className = "usertitle">Writer</p>
+                    </div>
+                    <div className="textRight">
+                        <div className="wrapper">
+                            <div className="userName">John Appleseed</div>
+                            <div className = "usertitle">Writer</div>
+                        </div>
                     </div>
                 </div>
                     
-                <div className="userReportsBlock">
-                 <ul className="userReports1">
-                	<li className="userReportTitle1"><NavLink to="/app">Home</NavLink></li>
-                	</ul>
-		                        
-                {/* Change to accordion menu */}
-
-	                	<Collapsible trigger = "Reports" className = "accordianMenu" triggerOpenedClassName="CustomTriggerCSS--open">
-		                    <ul className="userReports">
-                        <li className="userReportTitle"><NavLink to="/app/report1">Todos</NavLink></li>
-                        <li className="userReportTitle"><NavLink to="/app/report2">Posts</NavLink></li>
+                <div className="menuBlock">
+                    <ul className="menuSection">
+                        <li className="menuTitle">
+                            <i className="fa fa-home"></i>
+                            <NavLink to="/app">Home</NavLink>
+                        </li>
                     </ul>
-	                    
-	                	</Collapsible>
-                </div>
-                {/* Remove and merge into the above menu */}
-                <div className  = "userFooterBlock">
-                    <ul className = "userFooter">
-                        <li className = "user_help" ><i className="fa fa-info-circle" aria-hidden="true"></i><NavLink to="/help">Help</NavLink></li>
-                        <li className = "user_settings" ><i className="fa fa-cog" aria-hidden="true"></i> <NavLink to="/settings">Settings</NavLink> </li>
-                        <li className = "user_logout" ><i className="fa fa-power-off" aria-hidden="true"></i> <NavLink to="/logout">Logout</NavLink></li>
+                    {/* Accordion style for reports */}
+                    <Collapsible trigger="Reports" className="accordionMenu" transitionTime={400} easing={'cubic-bezier(0.175, 0.885, 0.32, 2.275)'} triggerOpenedClassName="accordionOpen">
+                        <ul className="userReports">
+                            <li className="userReportTitle"><NavLink to="/app/report1">Todos</NavLink></li>
+                            <li className="userReportTitle"><NavLink to="/app/report2">Posts</NavLink></li>
+                        </ul>
+                    </Collapsible>
+                    <ul className="menuSection">
+                        <li className="menuTitle">
+                        <i className="fa fa-info-circle" aria-hidden="true"></i>
+                            <NavLink to="/help">Help</NavLink>
+                        </li>
+                        <li className="menuTitle">
+                            <i className="fa fa-cog" aria-hidden="true"></i>
+                            <NavLink to="/settings">Settings</NavLink>
+                        </li>
+                        <li className="menuTitle">
+                            <i className="fa fa-power-off" aria-hidden="true"></i>
+                            <NavLink to="/logout">Logout</NavLink>
+                        </li>
                     </ul>
                 </div>
             </Col>
